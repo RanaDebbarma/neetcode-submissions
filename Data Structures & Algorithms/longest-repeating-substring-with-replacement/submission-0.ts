6a6d7b931e97d3cef5693ac7f876e3d1023fd406
RanaDebbarma <ranadebbarma2001@gmail.com>
2026-07-12 11:32:04 +0530

    Add: longest-repeating-substring-with-replacement - submission-0

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.ts
@@ -0,0 +1,46 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s: string, k: number): number {
+        let l = 0;
+        let r = 1;
+        let max_length = 0;
+
+        while (s.length && r < s.length) {
+            const current_window = s.substring(l, r + 1);
+            const max_freq = findMaxFreq(current_window);
+            const replacements = current_window.length - max_freq;
+
+            if (replacements <= k) {
+                if (max_length < current_window.length) max_length = current_window.length;
+                r++;
+            } else {
+                l++;
+            }
+        }
+
+        return max_length;
+
+        // Helper
+        function findMaxFreq(s: string) {
+            const hash = new Int32Array(26);
+            for (const ch of s) {
+                hash[ch.charCodeAt(0) - 65]++;
+            }
+
+            // extract max frequency;
+            let maxFreq = 0;
+            for (let i = 0; i < hash.length; i++) {
+                const freq = hash[i];
+                if (freq) {
+                    if (maxFreq < freq) maxFreq = freq;
+                }
+            }
+
+            return maxFreq;
+        }
+    }
+}

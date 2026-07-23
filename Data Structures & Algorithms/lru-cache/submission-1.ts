00169fb4d0a6895500bdfbf67bed893bd397f792
RanaDebbarma <ranadebbarma2001@gmail.com>
2026-07-24 00:19:18 +0530

    Add: lru-cache - submission-1

---
+++ Data Structures & Algorithms/lru-cache/submission-1.ts
@@ -0,0 +1,70 @@
+class CacheNode {
+  key: number;
+  value: number;
+  prev: CacheNode | null = null;
+  next: CacheNode | null = null;
+
+  constructor(key: number, value: number) {
+    this.key = key;
+    this.value = value;
+  }
+}
+
+class LRUCache {
+    private capacity: number;
+    private cache: Map<number, CacheNode>;
+    private head: CacheNode;
+    private tail: CacheNode;
+
+    constructor(capacity: number) {
+        this.capacity = capacity;
+        this.cache = new Map();
+
+        this.head = new CacheNode(0, 0);
+        this.tail = new CacheNode(0, 0);
+        this.head.next = this.tail;
+        this.tail.prev = this.head;
+    }
+
+  private remove(node: CacheNode) {
+    const prevNode = node.prev!;
+    const nextNode = node.next!;
+    prevNode.next = nextNode;
+    nextNode.prev = prevNode;
+  }
+
+  private addToTail(node: CacheNode) {
+    const mruNode = this.tail.prev!;
+    mruNode.next = node;
+    node.prev = mruNode;
+    node.next = this.tail;
+    this.tail.prev = node;
+  }
+
+  get(key: number): number {
+    const node = this.cache.get(key)!;
+
+    if (!node) return -1;
+
+    this.remove(node);
+    this.addToTail(node);
+
+    return node.value;
+  }
+
+  put(key: number, value: number): void {
+    if (this.cache.has(key)) {
+      this.remove(this.cache.get(key)!);
+    }
+
+    const newNode = new CacheNode(key, value);
+    this.cache.set(key, newNode);
+    this.addToTail(newNode);
+
+    if (this.cache.size > this.capacity) {
+      const lruNode = this.head.next!;
+      this.remove(lruNode);
+      this.cache.delete(lruNode.key);
+    }
+  }
+}

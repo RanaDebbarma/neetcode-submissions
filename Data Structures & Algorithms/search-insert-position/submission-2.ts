c28ca77d66d9534d8ef9b23e8a471a1213e036bb
RanaDebbarma <ranadebbarma2001@gmail.com>
2026-08-29 06:54:25 +0530

    Add: search-insert-position - submission-2

---
+++ Data Structures & Algorithms/search-insert-position/submission-2.ts
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    searchInsert(nums: number[], target: number): number {
+        let l = 0;
+        let r = nums.length - 1;
+
+        while (l <= r) {
+            const mid = l + ((r - l) >> 1);
+
+            if (nums[mid] === target) return mid;
+
+            if (nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+
+        return nums[r] > target ? r : r + 1;
+    }
+}
